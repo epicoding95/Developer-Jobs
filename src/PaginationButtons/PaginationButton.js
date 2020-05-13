27f4435ff9900db383ddda837d91a46c60d925cc
kelar crisp <kelcrisp007@icloud.com>
2020-05-13 16:34:02 -0700

    more styling

--- src/PaginationButtons/PaginationButton.js
+++ src/PaginationButtons/PaginationButton.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { JobContext } from '../Context/JobContext';
+import classes from './PaginationButton.module.css'
 
 const PaginationButtons = () => {
     const { jobs, setJobs, paginationJobs, setWhichPage, whichPage } = useContext(JobContext)
@@ -11,14 +12,17 @@ const PaginationButtons = () => {
         let buttonsLength = Math.ceil(paginationJobs.length / 10);
         const array = Array(buttonsLength).fill(1);
         const mapped = array.map((_, index) => {
-            return <button key={index} onClick={() => setWhichPage([index * 10, (index * 10) + 10])}>{index + 1}</button>
+            return <button
+                key={index}
+                className={classes.IndividualButton}
+                onClick={() => setWhichPage([index * 10, (index * 10) + 10])}>{index + 1}</button>
         })
         console.log(mapped, 'mapped buttons')
         return mapped
     }
 
     return (
-        <div>
+        <div className={classes.ButtonsContainer}>
             {paginationJobs && setPaginationButtons()}
         </div>
     );
